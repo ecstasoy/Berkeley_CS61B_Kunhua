d7360cbc4ed8bdd24df78d0a398ab65ba5841c0e
Kunhua Huang <huang.kunh@northeastern.edu>
2024-06-08 20:04:19 +0800

    lab7

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,164 @@
+package bstmap;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
+    private BSTNode root;
+
+    private class BSTNode {
+        private K key;
+        private V value;
+        private BSTNode left;
+        private BSTNode right;
+
+        public BSTNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public BSTMap() {
+        root = null;
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node.value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(BSTNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + size(node.left) + size(node.right);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private BSTNode put(BSTNode node, K key, V value) {
+        if (node == null) {
+            return new BSTNode(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+        inOrderTraversal(root, keys);
+        return keys;
+    }
+
+    private void inOrderTraversal(BSTNode node, Set<K> keys) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraversal(node.left, keys);
+        keys.add(node.key);
+        inOrderTraversal(node.right, keys);
+    }
+
+    @Override
+    public V remove(K key) {
+        V value = get(key);
+        root = remove(root, key);
+        return value;
+    }
+
+    private BSTNode remove(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = remove(node.left, key);
+        } else if (cmp > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            if (node.right == null) {
+                return node.left;
+            }
+            if (node.left == null) {
+                return node.right;
+            }
+            BSTNode temp = node;
+            node = min(temp.right);
+            node.right = removeMin(temp.right);
+            node.left = temp.left;
+        }
+        return node;
+    }
+
+    private BSTNode min(BSTNode node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    private BSTNode removeMin(BSTNode node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        V val = get(key);
+        if (val == null || !val.equals(value)) {
+            return null;
+        }
+        root = remove(root, key);
+        return val;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+
+}
