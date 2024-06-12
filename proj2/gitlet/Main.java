0ab71b46f30d47d4b3bb3e0d7f94012b480fc245
Kunhua Huang <huang.kunh@northeastern.edu>
2024-06-12 21:40:38 +0800

    AG 2

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,7 +1,5 @@
 package gitlet;
 
-import java.io.IOException;
-
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author Kunhua Huang
  */
@@ -11,79 +9,76 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        try {
-            if (args.length == 0) {
-                System.out.println("Please enter a command.");
-                System.exit(0);
-            }
-            String firstArg = args[0];
-            switch(firstArg) {
-                case "init":
-                    validArgs(args, 1);
-                    Repository.init();
-                    break;
-                case "add":
-                    validArgs(args, 2);
-                    Repository.add(args[1]);
-                    break;
-                case "commit":
-                    validArgs(args, 2);
-                    Repository.commit(args[1]);
-                    break;
-                case "rm":
-                    validArgs(args, 2);
-                    Repository.rm(args[1]);
-                    break;
-                case "log":
-                    validArgs(args, 1);
-                    Repository.log();
-                    break;
-                case "global-log":
-                    validArgs(args, 1);
-                    Repository.globalLog();
-                    break;
-                case "find":
-                    validArgs(args, 2);
-                    Repository.find(args[1]);
-                    break;
-                case "status":
-                    validArgs(args, 1);
-                    Repository.status();
-                    break;
-                case "checkout":
-                    if (args.length == 2) {
-                        Repository.checkoutBranch(args[1]);
-                    } else if (args.length == 3) {
-                        Repository.checkoutFile(args[2]);
-                    } else if (args.length == 4) {
-                        Repository.checkoutCommit(args[1], args[3]);
-                    } else {
-                        System.out.println("Incorrect operands.");
-                        System.exit(0);
-                    }
-                    break;
-                case "branch":
-                    validArgs(args, 2);
-                    Repository.branch(args[1]);
-                    break;
-                case "rm-branch":
-                    validArgs(args, 2);
-                    Repository.rmBranch(args[1]);
-                    break;
-                case "reset":
-                    validArgs(args, 2);
-                    Repository.reset(args[1]);
-                    break;
-                case "merge":
-                    validArgs(args, 2);
-                    Repository.merge(args[1]);
-                    break;
-            }
-        } catch (IOException e) {
-            System.out.println("IOException");
-            System.exit(1);
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
         }
-
+        String firstArg = args[0];
+        switch(firstArg) {
+            case "init":
+                validArgs(args, 1);
+                Repository.init();
+                break;
+            case "add":
+                validArgs(args, 2);
+                Repository.add(args[1]);
+                break;
+            case "commit":
+                validArgs(args, 2);
+                Repository.commit(args[1]);
+                break;
+            case "rm":
+                validArgs(args, 2);
+                Repository.rm(args[1]);
+                break;
+            case "log":
+                validArgs(args, 1);
+                Repository.log();
+                break;
+            case "global-log":
+                validArgs(args, 1);
+                Repository.globalLog();
+                break;
+            case "find":
+                validArgs(args, 2);
+                Repository.find(args[1]);
+                break;
+            case "status":
+                validArgs(args, 1);
+                Repository.status();
+                break;
+            case "checkout":
+                if (args.length == 2) {
+                    Repository.checkoutBranch(args[1]);
+                } else if (args.length == 3) {
+                    Repository.checkoutFile(args[2]);
+                } else if (args.length == 4) {
+                    Repository.checkoutCommit(args[1], args[3]);
+                } else {
+                    System.out.println("Incorrect operands.");
+                    System.exit(0);
+                }
+                break;
+            case "branch":
+                validArgs(args, 2);
+                Repository.branch(args[1]);
+                break;
+            case "rm-branch":
+                validArgs(args, 2);
+                Repository.rmBranch(args[1]);
+                break;
+            case "reset":
+                validArgs(args, 2);
+                Repository.reset(args[1]);
+                break;
+            case "merge":
+                validArgs(args, 2);
+                Repository.merge(args[1]);
+                break;
+            default:
+                System.out.println("No command with that name exists.");
+                System.exit(0);
+        }
     }
 
     private static void validArgs(String[] args, int numArgs) {
